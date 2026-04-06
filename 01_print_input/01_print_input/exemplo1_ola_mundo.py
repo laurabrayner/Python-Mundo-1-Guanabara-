872e2e651472504b574ab2ec7c9ca4f6274a2625
Laura Brayner <laurabekinha2@gmail.com>
2026-04-06 12:13:37 -0300

    Add files via upload

---
+++ 01_print_input/01_print_input/exemplo1_ola_mundo.py
@@ -0,0 +1,4 @@
+# Solicita o nome do usuário
+nome = input("Digite seu nome: ")
+# Exibe uma mensagem de boas-vindas personalizada
+print("Olá,", nome, "! Bem-vindo(a) ao curso de Python!")
---
+++ 01_print_input/01_print_input/exemplo2_data_nascimento.py
@@ -0,0 +1,8 @@
+# Solicita o dia de nascimento do usuário
+dia = int (input("Digite o dia em que você nasceu: "))
+# Solicita o mês de nascimento do usuário
+mes = int (input("Digite o mês em que você nasceu: "))
+# Solicita o ano de nascimento do usuário
+ano = int (input("Digite o ano em que você nasceu: "))
+# Exibe a data de nascimento formatada
+print (f"Sua data de nascimento é {dia}/{mes}/{ano} ")
